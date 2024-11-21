ef9568f0a2856fc6824e15754f686948ff5bb6d3
Edward Ofori <tetraed.llc@gmail.com>
2024-11-21 15:06:20 -0500

    Added code for Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,25 @@
+import '.components/Nav.css';
+
 const Nav = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li className="nav-item">
+          <a href="/" className="nav-link active">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="/saved" className="nav-link">Saved Candidates</a>
+        </li>
+      </ul>
+    </nav>
+  );
+  
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
+
     <div>Nav</div>
   )
 };
+
 
 export default Nav;
