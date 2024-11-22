ae80feea2b5663d20d469513478be0df8b87ee2b
Edward Ofori <tetraed.llc@gmail.com>
2024-11-21 19:15:56 -0500

    Added code for Candidate interfaces

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,23 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    readonly id: number | null;
+    readonly login: string | null;
+    readonly name?: string | null;
+    readonly location?: string | null;
+    readonly avatar_url: string | null;
+    readonly email?: string | null;
+    readonly html_url: string | null;
+    readonly company?: string | null;
+    
+    
+    
+//     login: string;
+//     name?: string;
+//     location?: string;
+//     avatar_url: string;
+//     email?: string;
+//     html_url: string;
+//     company?: string;
+  }
+  
+  
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,43 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
+const [candidates, setCandidates] = useState<Candidate[]>([]);
+
+const [error, setError] = useState<string>('');
+
+const [loading, setLoading] = useState<boolean>(false);
+
+const handleSearch = async () => {
+  setLoading(true);
+  try {
+    const data = await searchGithub();
+  setCandidates(data);
+  setLoading(false);
+  }
+  catch (error) {}
+  finally {
+    setLoading(false);
+  }
+  
+};
+
+
   return <h1>CandidateSearch</h1>;
 };
 
 export default CandidateSearch;
+
+
+
+
+
+// import { useState, useEffect } from 'react';
+// import { searchGithub, searchGithubUser } from './api/API';
+
+// const CandidateSearch = () => {
+//   return <h1>CandidateSearch</h1>;
+// };
+
+// export default CandidateSearch;
